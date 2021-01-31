fba01c86da3a25bf24fb05b45d5d0727323229f5
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2021-01-31 16:58:44 +0800

    添加appid

--- src/pages/user/invite/invite.vue
+++ src/pages/user/invite/invite.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="content">
     <view class="invite-wrapper" v-if="inviteUrl">
-      <uni-qrcode class="invite-qrcode" ref="qrcode" cid="invite-qr-canvas" :text="inviteUrl" :margin="10" makeOnLoad @makeComplete="qrcodeComplete" />
+      <!-- <uni-qrcode class="invite-qrcode" ref="qrcode" cid="invite-qr-canvas" :text="inviteUrl" :margin="10" makeOnLoad @makeComplete="qrcodeComplete" /> -->
       <view class="share-btn">
         <button type="primary" @click="shareLink">分享链接</button>
         <button type="primary" @click="shareImage">分享图片</button>
@@ -17,142 +17,142 @@
 </template>
 
 <script>
-  import {
-    mapState
-  } from 'vuex'
-  export default {
-    data() {
-      return {
-        inviteUrl: '',
-        qrcodeImagePath: '',
-        qrcodePath: '',
-        shareType: ''
-      }
-    },
-    onLoad() {
-      uni.showLoading({
-        title: '加载中...'
-      })
-      uniCloud.callFunction({
-        name: 'user-center',
-        data: {
-          action: 'getInviteCode'
-        },
-        success: (res) => {
-          console.log(res);
-          if (res.result.code === 0) {
-            // 这里请修改为真实的邀请页面url
-            this.inviteUrl = 'https://login.tpl.dcloud.net.cn/#/pages/user/invite-reg/invite-reg?invite_code=' + res.result.myInviteCode
-          } else {
-            uni.showModal({
-              content: '获取用户邀请码失败:' + res.result.msg,
-              showCancel: false
-            })
-          }
-        },
-        fail: (err) => {
+import {
+  mapState
+} from 'vuex'
+export default {
+  data () {
+    return {
+      inviteUrl: '',
+      qrcodeImagePath: '',
+      qrcodePath: '',
+      shareType: ''
+    }
+  },
+  onLoad () {
+    uni.showLoading({
+      title: '加载中...'
+    })
+    uniCloud.callFunction({
+      name: 'user-center',
+      data: {
+        action: 'getInviteCode'
+      },
+      success: (res) => {
+        console.log(res);
+        if (res.result.code === 0) {
+          // 这里请修改为真实的邀请页面url
+          this.inviteUrl = 'https://login.tpl.dcloud.net.cn/#/pages/user/invite-reg/invite-reg?invite_code=' + res.result.myInviteCode
+        } else {
           uni.showModal({
-            content: '获取用户邀请码失败，请稍后再试',
+            content: '获取用户邀请码失败:' + res.result.msg,
+            showCancel: false
+          })
+        }
+      },
+      fail: (err) => {
+        uni.showModal({
+          content: '获取用户邀请码失败，请稍后再试',
+          showCancel: false
+        })
+      },
+      complete () {
+        uni.hideLoading()
+      }
+    })
+  },
+  methods: {
+    copyInviteUrl () {
+      uni.setClipboardData({
+        data: this.inviteUrl
+      })
+    },
+    qrcodeComplete (path) {
+      this.qrcodeImagePath = path
+    },
+    shareLink () {
+      this.shareType = 'link'
+      this.$refs.popupShare.open()
+    },
+    shareImage () {
+      this.shareType = 'image'
+      this.$refs.popupShare.open()
+    },
+    select ({
+      item
+    }) {
+      const shareTitle = '登录模板',
+        shareSummary = 'DCloud邀请您试用登录模板'
+      let params = {}
+      switch (`${ this.shareType }_${ item.name }`) {
+        case 'link_weixin':
+          params = {
+            type: 1,
+            summary: this.inviteUrl,
+            scene: 'WXSceneSession'
+          }
+          break;
+        case 'link_qq':
+          params = {
+            type: 1,
+            title: shareTitle,
+            summary: shareSummary,
+            href: this.inviteUrl
+          }
+          break;
+        case 'link_more':
+          uni.setClipboardData({
+            data: this.inviteUrl
+          })
+          return
+        case 'image_weixin':
+          params = {
+            type: 2,
+            imageUrl: this.qrcodeImagePath,
+            scene: 'WXSceneSession'
+          }
+          break;
+        case 'image_qq':
+          params = {
+            type: 2,
+            imageUrl: this.qrcodeImagePath
+          }
+          break;
+        case 'image_more':
+          uni.shareWithSystem({
+            type: 'image',
+            imageUrl: this.qrcodeImagePath
+          })
+          return
+        default:
+          break;
+      }
+      console.log(params);
+      uni.share({
+        provider: item.name,
+        ...params,
+        success () {
+          uni.showModal({
+            content: '分享成功',
             showCancel: false
           })
         },
-        complete() {
-          uni.hideLoading()
+        fail (err) {
+          uni.showModal({
+            content: '分享失败：' + err.errMsg,
+            showCancel: false
+          })
         }
       })
-    },
-    methods: {
-      copyInviteUrl() {
-        uni.setClipboardData({
-          data: this.inviteUrl
-        })
-      },
-      qrcodeComplete(path) {
-        this.qrcodeImagePath = path
-      },
-      shareLink() {
-        this.shareType = 'link'
-        this.$refs.popupShare.open()
-      },
-      shareImage() {
-        this.shareType = 'image'
-        this.$refs.popupShare.open()
-      },
-      select({
-        item
-      }) {
-        const shareTitle = '登录模板',
-          shareSummary = 'DCloud邀请您试用登录模板'
-        let params = {}
-        switch (`${this.shareType}_${item.name}`) {
-          case 'link_weixin':
-            params = {
-              type: 1,
-              summary: this.inviteUrl,
-              scene: 'WXSceneSession'
-            }
-            break;
-          case 'link_qq':
-            params = {
-              type: 1,
-              title: shareTitle,
-              summary: shareSummary,
-              href: this.inviteUrl
-            }
-            break;
-          case 'link_more':
-            uni.setClipboardData({
-              data: this.inviteUrl
-            })
-            return
-          case 'image_weixin':
-            params = {
-              type: 2,
-              imageUrl: this.qrcodeImagePath,
-              scene: 'WXSceneSession'
-            }
-            break;
-          case 'image_qq':
-            params = {
-              type: 2,
-              imageUrl: this.qrcodeImagePath
-            }
-            break;
-          case 'image_more':
-            uni.shareWithSystem({
-              type: 'image',
-              imageUrl: this.qrcodeImagePath
-            })
-            return
-          default:
-            break;
-        }
-        console.log(params);
-        uni.share({
-          provider: item.name,
-          ...params,
-          success() {
-            uni.showModal({
-              content: '分享成功',
-              showCancel: false
-            })
-          },
-          fail(err) {
-            uni.showModal({
-              content: '分享失败：' + err.errMsg,
-              showCancel: false
-            })
-          }
-        })
 
-      },
-      toInvitedUser() {
-        uni.navigateTo({
-          url: '/pages/user/invited-user/invited-user'
-        })
-      }
+    },
+    toInvitedUser () {
+      uni.navigateTo({
+        url: '/pages/user/invited-user/invited-user'
+      })
     }
   }
+}
 </script>
 
 <style>
